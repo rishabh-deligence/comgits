dad9d600661e05fc158b87d5bb563a2e43a839bf
Rishabh Jain <rishabh.deligence@gmail.com>
2020-01-13 18:05:39 +0530

    add refresh indicator and final UI

--- lib/main.dart
+++ lib/main.dart
@@ -59,6 +59,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   int pageNumber = 1;
+  bool isDataAvailable = true;
   var items = List();
   bool isLoading = false;
   @override
@@ -69,10 +70,13 @@ class _MyAppState extends State<MyApp> {
 
   Future<List> fetchCommit() async {
     final response = await http.get(
-        'https://api.github.com/repos/facebook/react-native/commits?page=$pageNumber&per_page=10');
+        'https://api.github.com/repos/rishabh6072/BurgerTheBuilder/commits?page=$pageNumber&per_page=10');
     if (response.statusCode == 200) {
       pageNumber++;
       List responseJson = json.decode(response.body);
+
+      if (responseJson.length < 10) isDataAvailable = false;
+
       items.addAll(responseJson.map((m) => new Commit.fromJson(m)));
       setState(() {
         isLoading = false;
@@ -83,16 +87,25 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
+  Future<List> refreshCommit() async {
+    items = List();
+    pageNumber = 1;
+    isDataAvailable = true;
+    fetchCommit();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Comgits',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Comgits'),
+          backgroundColor: Colors.black87,
+          title: const Text('Comgits'),
         ),
         body: Column(
           children: <Widget>[
@@ -101,7 +114,8 @@ class _MyAppState extends State<MyApp> {
                 onNotification: (ScrollNotification scrollInfo) {
                   if (!isLoading &&
                       scrollInfo.metrics.pixels ==
-                          scrollInfo.metrics.maxScrollExtent) {
+                          scrollInfo.metrics.maxScrollExtent &&
+                      isDataAvailable) {
                     fetchCommit();
                     setState(() {
                       isLoading = true;
@@ -109,74 +123,77 @@ class _MyAppState extends State<MyApp> {
                   }
                   return false;
                 },
-                child: ListView(
-                  children: items
-                      .map((commit) => Container(
-                            padding: EdgeInsets.all(10),
-                            child: Card(
-                              elevation: 12.0,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(12.0),
+                child: RefreshIndicator(
+                  onRefresh: refreshCommit,
+                  child: ListView(
+                    children: items
+                        .map((commit) => Container(
+                              padding: const EdgeInsets.all(10),
+                              child: Card(
+                                elevation: 12.0,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(12.0),
+                                ),
+                                child: Padding(
+                                    padding: const EdgeInsets.all(12.0),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.stretch,
+                                      children: <Widget>[
+                                        textSection(
+                                            'Unique ID', '${commit.sha}'),
+                                        textSection('Commit',
+                                            '${commit.commit.message}'),
+                                        textSection('Committer Name',
+                                            '${commit.commit.committer.name}'),
+                                        textSection('Committer email',
+                                            '${commit.commit.committer.email}'),
+                                        textSection('Commit Date',
+                                            '${commit.commit.committer.date}'),
+                                      ],
+                                    )),
                               ),
-                              child: Padding(
-                                  padding: const EdgeInsets.all(12.0),
-                                  child: Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: <Widget>[
-                                      RichText(
-                                        text: TextSpan(
-                                            style: TextStyle(
-                                                color: Colors.black87,
-                                                fontSize: 18),
-                                            children: [
-                                              TextSpan(
-                                                text: 'Unique ID : ',
-                                                style: TextStyle(
-                                                    fontWeight:
-                                                        FontWeight.w700),
-                                              ),
-                                              TextSpan(text: '${commit.sha}  ')
-                                            ]),
-                                      ),
-                                      SizedBox(height: 5),
-                                      RichText(
-                                        text: TextSpan(
-                                            style: TextStyle(
-                                                color: Colors.black87,
-                                                fontSize: 18),
-                                            children: [
-                                              TextSpan(
-                                                text: 'Committer : ',
-                                                style: TextStyle(
-                                                    fontWeight:
-                                                        FontWeight.w700),
-                                              ),
-                                              TextSpan(
-                                                  text:
-                                                      '${commit.commit.committer.email} ')
-                                            ]),
-                                      ),
-                                    ],
-                                  )),
-                            ),
-                          ))
-                      .toList(),
+                            ))
+                        .toList(),
+                  ),
                 ),
               ),
             ),
             Container(
               height: isLoading ? 50.0 : 0,
               color: Colors.transparent,
-              child: Center(
-                child: new CircularProgressIndicator(),
+              child: const Center(
+                child: const CircularProgressIndicator(),
               ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  textSection(String title, String subtitle) {
+    return subtitle == null || subtitle == '' || subtitle.isEmpty
+        ? Container()
+        : Container(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Text(
+                  title,
+                  style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w700),
+                ),
+                Text(subtitle,
+                    style: TextStyle(color: Colors.black87, fontSize: 18)),
+              ],
+            ),
+          );
   }
 }
